09e8c1fd47ee7e157f8f09b0cbd931fb99e512a6
Roboramv2 <sriramkraja@gmail.com>
2021-04-22 00:05:11 +0530

    updated python code

--- picam/cam.py
+++ picam/cam.py
@@ -1,5 +1,12 @@
 import time
 from picamera import PiCamera
 camera = PiCamera()
-time.sleep(0.0005)
+camera.resolution = (1024, 768)
+time.sleep(1)
+
+#capture images using the camera class
 camera.capture('/home/pi/Pictures/snap.jpg')
+
+#resize and capture
+camera.capture('/home/pi/Pictures/resized.jpg', resize=(320, 240))
+
